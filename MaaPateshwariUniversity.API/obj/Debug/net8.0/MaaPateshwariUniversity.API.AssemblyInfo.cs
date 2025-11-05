9e2adeaacdcf55d4691d986d90cf97fa8ae6aa19
yogeshsingh756 <ythakursingh98@gmail.com>
2025-11-05 17:22:28 +0700

    Clean workspace – Removed .vs, bin, obj, Copilot files & added .gitignore

--- MaaPateshwariUniversity.API/obj/Debug/net8.0/MaaPateshwariUniversity.API.AssemblyInfo.cs
+++ MaaPateshwariUniversity.API/obj/Debug/net8.0/MaaPateshwariUniversity.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaaPateshwariUniversity.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+175fbb709bed7fab79d8d6e79825a06968558336")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaaPateshwariUniversity.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaaPateshwariUniversity.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MaaPateshwariUniversity.Infrastructure/obj/Debug/net8.0/MaaPateshwariUniversity.Infrastructure.AssemblyInfo.cs
+++ MaaPateshwariUniversity.Infrastructure/obj/Debug/net8.0/MaaPateshwariUniversity.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaaPateshwariUniversity.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+175fbb709bed7fab79d8d6e79825a06968558336")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaaPateshwariUniversity.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaaPateshwariUniversity.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
